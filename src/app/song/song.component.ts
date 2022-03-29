0054f894dad29607cbf78eb60b253c7b1ef6066d
nickmaglowsch <nickmaglowsch3@gmail.com>
2022-03-29 18:49:27 -0300

    2 files modified

--- src/app/song/song.component.ts
+++ src/app/song/song.component.ts
@@ -1,15 +1,29 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { SongModel } from "./song.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { SongModel } from './song.model';
 
 @Component({
-  selector: "app-song",
-  templateUrl: "./song.component.html",
-  styleUrls: ["./song.component.scss"]
+  selector: 'app-song',
+  templateUrl: './song.component.html',
+  styleUrls: ['./song.component.scss'],
 })
 export class SongComponent implements OnInit {
   @Input() currentSong: SongModel;
-
+  currentView: 'list' | 'card' = 'list';
   constructor() {}
 
   ngOnInit(): void {}
+
+  changeView(view: 'list' | 'card') {
+    this.currentView = view;
+  }
+
+  isPrime(n: number) {
+    if (n <= 1) return false;
+
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) return false;
+    }
+
+    return true;
+  }
 }
