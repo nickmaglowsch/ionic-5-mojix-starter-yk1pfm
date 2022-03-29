cbadc7b21da396519d6279f2b8260b4befbb3925
nickmaglowsch <nickmaglowsch3@gmail.com>
2022-03-29 20:46:15 -0300

    1 file modified

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -18,22 +18,21 @@ export class ApiService {
     limit: number = 50
   ): Observable<SongResponse> {
     const path = `https://itunes.apple.com/search?term=${search}&media=${media}&limit=${limit}`;
-    return this.http.get<SongResponse>(path);
-    // .pipe
-    // retryWhen((errors) =>
-    //   errors.pipe(
-    //     //log error message
-    //     tap((error) => console.log(`api failed with error`, error)),
-    //     scan((acc, error) => {
-    //       if (acc > 2) throw error;
-    //       console.log('attempt ' + acc);
-    //       return acc + 1;
-    //     }, 1),
-    //     take(2),
-    //     //restart in 6 seconds
-    //     delayWhen((val) => timer(val * 2000))
-    //   )
-    // )
-    // ();
+    return this.http.get<SongResponse>(path).pipe(
+      retryWhen((errors) =>
+        errors.pipe(
+          //log error message
+          tap((error) => console.log(`api failed with error`, error)),
+          scan((acc, error) => {
+            if (acc > 2) throw error;
+            console.log('attempt ' + acc);
+            return acc + 1;
+          }, 1),
+          take(2),
+          //restart in 6 seconds
+          delayWhen((val) => timer(val * 2000))
+        )
+      )
+    );
   }
 }
